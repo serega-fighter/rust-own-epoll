382dd38c81b177a97220968281da93cb672fd952
Serega <26937163+serega-fighter@users.noreply.github.com>
2024-03-20 17:31:23 +0300

    own epoll wrapper WIP

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,72 @@
+use std::{io::{self, Read, Result, Write}, net::TcpStream};
+use ffi::Event;
+use poll::Poll;
+
 mod ffi;
 mod poll;
+
+fn get_req(path: &str) -> Vec<u8> {
+    let text = format!(
+        "GET {path} HTTP/1.1\r\n\
+        Host: localhost\r\n\
+        Connection: close\r\n\
+        \r\n"
+    );
+    return text.into_bytes();
+}
+
+fn handle_events(events: &[Event], streams: &mut [TcpStream]) -> Result<usize> {
+    let mut handled_events = 0;
+    for event in events {
+        let index = event.token();
+        let mut data = vec![0u8; 4096];
+        loop {
+            match streams[index].read(&mut data) {
+                Ok(n) if n == 0 => {
+                    handled_events += 1;
+                    break;
+                }
+                Ok(n) => {
+                    let txt = String::from_utf8_lossy(&data[..n]);
+                    println!("RECEIVED: {:?}", event);
+                    println!("{txt}\n------\n");
+                }
+                // Not ready to read in a non-blocking manner. This could
+                // happen even if the event was reported as ready
+                Err(e) if e.kind() == io::ErrorKind::WouldBlock => break,
+                Err(e) => return Err(e),
+            }
+        }
+    }
+    Ok(handled_events)
+}
+
+fn main() -> Result<()> {
+    let mut poll = Poll::new()?;
+    let n_events = 5;
+    let mut streams = vec![];
+    let addr = "localhost:8080";
+    for i in 0..n_events {
+        let delay = (n_events - i) * 1000;
+        let url_path = format!("/{delay}/request-{i}");
+        let request = get_req(&url_path);
+        let mut stream = std::net::TcpStream::connect(addr)?;
+        stream.set_nonblocking(true)?;
+        stream.write_all(request.as_slice())?;
+        poll.registry()
+            .register(&stream, i, ffi::EPOLLIN | ffi::EPOLLET)?;
+        streams.push(stream);
+    }
+    let mut handled_events = 0;
+    while handled_events < n_events {
+        let mut events = Vec::with_capacity(10);
+        poll.poll(&mut events, None)?;
+        if events.is_empty() {
+            println!("TIMEOUT (OR SPURIOUS EVENT NOTIFICATION)");
+            continue;
+        }
+        handled_events += handle_events(&events, &mut streams)?;
+    }
+    println!("FINISHED");
+    Ok(())
+}
--- src/poll.rs
+++ src/poll.rs
@@ -1,5 +1,7 @@
 use std::io;
+use std::io::Error;
 use std::net::TcpStream;
+use std::os::fd::AsRawFd;
 use crate::ffi;
 
 type Events = Vec<ffi::Event>;
@@ -9,21 +11,32 @@ pub struct Poll {
 }
 
 impl Poll {
-    pub fn new() -> Result<Self> {
+    pub fn new() -> Result<Self, Error> {
         let res = unsafe { ffi::epoll_create(1) };
         if res < 0 {
-            return Err(io::Error::last_os_error());
+            return Err(Error::last_os_error());
         }
 
         Ok(Self {
             registry: Registry {raw_fd: res},
         })
     }
+
     pub fn registry(&self) -> &Registry {
         &self.registry
     }
-    pub fn poll(&mut self, events: &mut Events, timeout: Option<i32>) -> Result<()> {
-        todo!()
+
+    pub fn poll(&mut self, events: &mut Events, timeout: Option<i32>) -> Result<(), Error> {
+        let fd = self.registry.raw_fd;
+        let timeout = timeout.unwrap_or(-1);
+        let max_events = events.capacity() as i32;
+        let res = unsafe { ffi::epoll_wait(fd, events.as_mut_ptr(), max_events, timeout)};
+
+        if res < 0 {
+            return Err(Error::last_os_error())
+        }
+        unsafe { events.set_len(res as usize) };
+        Ok(())
     }
 }
 
@@ -32,13 +45,28 @@ pub struct Registry {
 }
 
 impl Registry {
-    pub fn register(&self, source: &TcpStream, token: usize, interests: i32) -> Result<()> {
-        todo!()
+    pub fn register(&self, source: &TcpStream, token: usize, interests: i32) -> Result<(), Error> {
+        let mut event = ffi::Event {
+            events: interests as u32,
+            epoll_data: token,
+        };
+        let op = ffi::EPOLL_CTL_ADD;
+        let res = unsafe {
+            ffi::epoll_ctl(self.raw_fd, op, source.as_raw_fd(), &mut event)
+        };
+        if res < 0 {
+            return Err(io::Error::last_os_error());
+        }
+        Ok(())
     }
 }
 
 impl Drop for Registry {
     fn drop(&mut self) {
-        todo!()
+        let res = unsafe { ffi::close(self.raw_fd) };
+        if res < 0 {
+            let err = io::Error::last_os_error();
+            eprintln!("ERROR: {err:?}");
+        }
     }
 }
